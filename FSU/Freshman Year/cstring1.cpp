086af47cb635ca9502c4f8f4534140473abfadd5
marshall copeland <31311876+mcop77@users.noreply.github.com>
2021-10-20 01:47:35 -0400

    Added CString1.cpp
    Practice with C strings

---
+++ FSU/Freshman Year/cstring1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char name[] = "Marshall";
+	
+	cout << name << endl;
+	
+	char stuff[4] = {'i' , 'j', 'k', 'l'};
+	
+	cout << stuff << endl;
+	
+	
+	
+	return 0;
+}
