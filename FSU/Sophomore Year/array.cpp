f46dc2a2936a957ea0a3a1fc5fa53a49e1c06938
marshall copeland <31311876+mcop77@users.noreply.github.com>
2021-12-18 21:41:37 -0500

    Created Array.cpp
    Messing around with arrays

---
+++ FSU/Sophomore Year/array.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(int x = 0; x < 5; x++)
+	{
+		for(int y = 0; y < 5; y++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				cout << "X ";
+				
+				if(j == 4)
+				{
+					cout << "\t";
+				}
+			}
+			
+			for(int k = 0; k < 5; k++)
+			{
+				cout << "X ";
+				
+				if(k == 4)
+				{
+					cout << "\t";
+				}
+			}
+			
+			for(int l = 0; l < 5; l++)
+			{
+				cout << "X ";
+				
+				if(l == 4)
+				{
+					cout << "\t";
+				}
+			}
+			
+			for(int l = 0; l < 5; l++)
+			{
+				cout << "X ";
+				
+				if(l == 4)
+				{
+					cout << "\t";
+				}
+			}
+			
+			for(int z = 0; z < 5; z++)
+			{
+				cout << "X ";
+				
+				if(z == 4)
+				{
+					cout << "\t";
+				}
+			}
+					
+			cout << "\n";
+		}	
+		
+		cout << "\n";
+		cout << "\n";
+	}
+	
+	return 0;
+}
