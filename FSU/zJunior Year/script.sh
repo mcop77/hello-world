63cccdf760b5da534df46910ffdb7244b185d4c5
marshall copeland <31311876+mcop77@users.noreply.github.com>
2022-03-16 22:31:22 -0400

    Created Script.sh
    This is the driver program for Map.py

---
+++ FSU/zJunior Year/script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+cd /media/sf_Germany
+
+echo "Loading Images..."
+
+
+for filename in *; do
+	exiftool $filename >> /home/mcop/Desktop/python/ImageMap/exifOutput.txt
+done
+
+echo "Processing Images..."
+
+cd /home/mcop/Desktop/python/ImageMap
+python3 map.py
+google-chrome map.html
+rm exifOutput.txt
+rm map.html
